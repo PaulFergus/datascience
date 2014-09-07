30c1114938b517e36aac9a49386f29b96697ea3a
Paul Fergus <p.fergus@ljmu.ac.uk>
2014-09-07 14:05:31 +0100

    update datascience modules

---
+++ 3_getcleandata/courseproject/run_analysis.R
@@ -0,0 +1,19 @@
+# 1. Merges the training and the test sets to create one data set.
+
+#Need to do
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+
+#Need to do
+
+# 3. Uses descriptive activity names to name the activities in the data set
+
+#Need to do 
+
+# 4. Appropriately labels the data set with descriptive variable names. 
+
+#Need to do
+
+# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+#Need to do
